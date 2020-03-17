91d70433281c6917c7c56b3310f301b848567c5a
Smundada-27 <snehalmundada7@gmail.com>
2020-03-17 19:19:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8kr3jq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8kr3jq)
---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+}
+function log(message){
+  console.log(message);
+}
+var message = 'Hello World'
+log(message);
+var num =1;
+let count =2;
+function doSomething()
+{
+  for(let i=0; i<5; i++){
+    console.log(i);
+  }
+  console.log('Finally: ' + i);
+}
+doSomething();
+let a;
+a=1;
+a=true;
+a='a';
+let b:number;
+let c: boolean;
+let d: any;
+let e : number[] = [1,2,3];
+let f : any[] = [1, true, 'a' ,false];
+
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
+enum Color{Red = 0,Green = 1 ,Blue = 2};
+let backgrounColor =Color.Red;
+
